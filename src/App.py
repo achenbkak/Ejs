48d197b338a1971ee80de1abf57c2f0e362ef6a7
achenbkak <achenbkak@gmail.com>
2026-09-25 16:21:08 +0200

    sept 25

--- src/App.py
+++ src/App.py
@@ -1,2 +1,19 @@
 print("Hello world")
 print("mme gusta la caca")
+message = "a"
+message2 = "b"
+full_message = f"{message} {message2}"
+x, y, z = 0,1,2
+print(x,y,z)
+num = 1_234_56
+print("\n",num)
+
+def fibo():
+    lista = [0,1]
+    for i in range(10):
+        suma = lista[len(lista)-2] + lista[len(lista)-1]
+        lista.append(suma)
+
+    print(lista)
+    
+fibo()
---
+++ src/EjerciciosBucles.py
@@ -0,0 +1,50 @@
+import random
+
+#Ejercicio 1
+listEnteros = []
+listEnteros2 = []
+contador = 0
+numAux = 0
+numAux2 = 0
+
+for i in range(15):
+    numAux += 1
+    listEnteros.append(numAux)
+
+print(listEnteros)
+
+while (contador<20):
+    numAux2 += 1
+    listEnteros2.append(numAux2)
+    contador +=1
+
+print (listEnteros2)
+
+#Ejercicio 2
+listaRandom = []
+numeroRandom = 0
+
+for i in range(15):
+    numeroRandom = random.randint(0,100)
+    listaRandom.append(numeroRandom)
+
+print(listaRandom)
+
+listaRandom2 = []
+numeroRandom2 = 0
+contadorRandom = 0;
+while(contadorRandom<13):
+    numeroRandom2 = random.uniform(3,5)
+    numeroRandom2 = round(numeroRandom2,2)
+    listaRandom2.append(numeroRandom2)
+    contadorRandom += 1
+
+print(listaRandom2)
+
+listaRandom3 = []
+valorRandom = ""
+for i in range(4):
+    valorRandom = random.choice(['Izquierda', 'Derecha', 'Arriba', 'Abajo'])
+    listaRandom3.append(valorRandom)
+
+print(listaRandom3)
